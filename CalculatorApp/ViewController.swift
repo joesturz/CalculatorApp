0e2cb3ee3208fa5b19631ae12351d6d47371d930
Joe Sturzenegger <joesturz84@gmail.com>
2017-04-17 20:57:41 -0600

    added pi and square root, also display now works and appends digits

--- CalculatorApp/ViewController.swift
+++ CalculatorApp/ViewController.swift
@@ -10,12 +10,43 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet var label: UILabel!
+    @IBOutlet weak var display: UILabel!
+    
+    var userIsInTheMiddleOfTyping = false
     
     @IBAction func touchDigit(_ sender: UIButton) {
         let digit = sender.currentTitle!
-        label.text = digit
+        if userIsInTheMiddleOfTyping {
+            let textCurrentlyInDisplay = display.text!
+            display.text = textCurrentlyInDisplay + digit
+        } else {
+            display.text = digit
+            userIsInTheMiddleOfTyping = true
+        }
     }
-
+    
+    var displayValue: Double {
+        get {
+            return Double(display.text!)!
+        }
+        set {
+            display.text = String(newValue)
+        }
+    }
+    
+    @IBAction func preformOperation(_ sender: UIButton) {
+        if let mathmaticalSymbol = sender.currentTitle {
+            userIsInTheMiddleOfTyping = false
+            switch mathmaticalSymbol {
+            case "π":
+                displayValue = Double.pi
+            case "√":
+                displayValue = sqrt(displayValue)
+            default:
+                break
+            }
+        }
+    }
+    
 }
 
