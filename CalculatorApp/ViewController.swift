e0f84820ea1582498b39ec2e61df9a57375e9f01
Joe Sturzenegger <joesturz84@gmail.com>
2017-04-18 22:27:55 -0600

    Added several functions, learned about closures, switches and structs. Set up for remaining implimentation.

--- CalculatorApp/ViewController.swift
+++ CalculatorApp/ViewController.swift
@@ -9,44 +9,47 @@
 import UIKit
 
 class ViewController: UIViewController {
+  
+  @IBOutlet weak var display: UILabel!
+  
+  var userIsInTheMiddleOfTyping = false
+  
+  @IBAction func touchDigit(_ sender: UIButton) {
+    let digit = sender.currentTitle!
+    if userIsInTheMiddleOfTyping {
+      let textCurrentlyInDisplay = display.text!
+      display.text = textCurrentlyInDisplay + digit
+    } else {
+      display.text = digit
+      userIsInTheMiddleOfTyping = true
+    }
+  }
+  
+  var displayValue: Double {
+    get {
+      return Double(display.text!)!
+    }
+    set {
+      display.text = String(newValue)
+    }
+  }
+  
+  private var brain = CalculatorBrain()
+  
+  @IBAction func preformOperation(_ sender: UIButton) {
     
-    @IBOutlet weak var display: UILabel!
-    
-    var userIsInTheMiddleOfTyping = false
-    
-    @IBAction func touchDigit(_ sender: UIButton) {
-        let digit = sender.currentTitle!
-        if userIsInTheMiddleOfTyping {
-            let textCurrentlyInDisplay = display.text!
-            display.text = textCurrentlyInDisplay + digit
-        } else {
-            display.text = digit
-            userIsInTheMiddleOfTyping = true
-        }
+    if userIsInTheMiddleOfTyping {
+      brain.setOperand(displayValue)
+      userIsInTheMiddleOfTyping = false
     }
     
-    var displayValue: Double {
-        get {
-            return Double(display.text!)!
-        }
-        set {
-            display.text = String(newValue)
-        }
+    if let mathmaticalSymbol = sender.currentTitle {
+      brain.performOperation(mathmaticalSymbol)
     }
-    
-    @IBAction func preformOperation(_ sender: UIButton) {
-        if let mathmaticalSymbol = sender.currentTitle {
-            userIsInTheMiddleOfTyping = false
-            switch mathmaticalSymbol {
-            case "π":
-                displayValue = Double.pi
-            case "√":
-                displayValue = sqrt(displayValue)
-            default:
-                break
-            }
-        }
+    if let result = brain.result {
+      displayValue = result
     }
-    
+  }
+  
 }
 
